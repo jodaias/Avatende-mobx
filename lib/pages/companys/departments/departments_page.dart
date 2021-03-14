1de65fa52970f9ee5de8d1005ba8163e025c8111
Jodaías Barreto da Silva <jodaias.silva@iatec.com>
2021-03-14 14:40:58 -0300

    resolvido bug do drawer no department

--- lib/pages/companys/departments/departments_page.dart
+++ lib/pages/companys/departments/departments_page.dart
@@ -90,8 +90,8 @@ class _DepartmentsPageState extends State<DepartmentsPage> {
             })
           ],
         ),
-        drawer: appStore.userViewModel.userType == "Atendente" ||
-                appStore.userViewModel.userType == "Atendente-Dev"
+        drawer: appStore.userViewModel.userType == "Admin" ||
+                appStore.userViewModel.userType == "Admin-Dev"
             ? CustomDrawer()
             : null,
         body: Container(
